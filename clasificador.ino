984ede2785d09e4ed5906fa98c71b8dda1a782c5
Nery-Vargas-Marco <50597992+Nery-Vargas-Marco@users.noreply.github.com>
2019-08-15 07:41:04 -0500

    Add files via upload

---
+++ clasificador.ino
@@ -0,0 +1,106 @@
+//
+// Cableado de TCS3200 a Arduino
+//
+#define S0 8
+#define S1 9
+#define S2 12
+#define S3 11
+#define salidaSensor 10
+ #include <Servo.h>
+ Servo myservo; 
+// Para guardar las frecuencias de los fotodiodos
+int frecuenciaRojo = 0;
+int frecuenciaVerde = 0;
+int frecuenciaAzul = 0;
+int colorRojo;
+int colorVerde;
+int colorAzul;
+int pos = 0; 
+ 
+void setup() {
+  // Definiendo las Salidas
+  Serial.begin(9600);
+  myservo.attach(13);  // attaches the servo on pin 9 to the servo object
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  
+  // Definiendo salidaSensor como entrada
+  pinMode(salidaSensor, INPUT);
+  
+  // Definiendo la escala de frecuencia a 20%
+  digitalWrite(S0,HIGH);
+  digitalWrite(S1,LOW);
+  
+   // Iniciar la comunicacion serie 
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  // Definiendo la lectura de los fotodiodos con filtro rojo
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,LOW);
+  
+  // Leyendo la frecuencia de salida del sensor
+  frecuenciaRojo = pulseIn(salidaSensor, LOW);
+ 
+  // Mapeando el valor de la frecuencia del ROJO (RED = R) de 0 a 255
+  // Usted debe colocar los valores que registro. Este es un ejemplo: 
+  // colorRojo = map(frecuenciaRojo, 70, 120, 255,0);
+  colorRojo = map(frecuenciaRojo, 36, 150, 255,0);
+  
+  // Mostrando por serie el valor para el rojo (R = Red)
+  //Serial.print("R = ");
+  //Serial.print(colorRojo);
+  delay(100);
+  
+  // Definiendo la lectura de los fotodiodos con filtro verde
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,HIGH);
+  
+  // Leyendo la frecuencia de salida del sensor
+  frecuenciaVerde = pulseIn(salidaSensor, LOW);
+ 
+  // Mapeando el valor de la frecuencia del VERDE (GREEN = G) de 0 a 255
+  // Usted debe colocar los valores que registro. Este es un ejemplo: 
+  // colorVerde = map(frecuenciaVerde, 100, 199, 255,0);
+  colorVerde = map(frecuenciaVerde, 70, 230, 255,0);
+ 
+  // Mostrando por serie el valor para el verde (G = Green)
+  //Serial.print("V = ");
+  //Serial.print(colorVerde);
+  delay(100);
+ 
+  // Definiendo la lectura de los fotodiodos con filtro azul
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,HIGH);
+  
+  // Leyendo la frecuencia de salida del sensor
+  frecuenciaAzul = pulseIn(salidaSensor, LOW);
+ 
+  // Mapeando el valor de la frecuencia del AZUL (AZUL = B) de 0 a 255
+  // Usted debe colocar los valores que registro. Este es un ejemplo: 
+  // colorAzul = map(frecuenciaAzul, 38, 84, 255, 0);
+  colorAzul = map(frecuenciaAzul, 47, 160, 255, 0);
+  
+  // Mostrando por serie el valor para el azul (B = Blue)
+  //Serial.print("B = ");
+  //Serial.print(colorAzul);
+  delay(100);
+ 
+  // Comprobar cual es el color detectado y mostrarlo
+  // con un mensaje en el monitor serie
+  if(colorRojo > colorVerde && colorRojo > colorAzul){
+      Serial.println(" - Detectado ROJO");
+      myservo.write(22);
+  }
+  if(colorVerde > colorRojo && colorVerde > colorAzul){
+    Serial.println(" - Detectado Amarillo");
+    myservo.write(67);
+  }
+  if(colorAzul > colorRojo && colorAzul > colorVerde){
+    Serial.println(" - Detectado AZUL");
+     myservo.write(112);
+  }
+}
